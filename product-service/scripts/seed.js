9313baef8dd5b4cc2f7069c1c611fa7c40d3404b
dev-sd092 <sanketdesai092@gmail.com>
2026-03-04 06:05:39 +0000

    Fix seed script model path

--- product-service/scripts/seed.js
+++ product-service/scripts/seed.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const Product = require('../models/Product');
-const Category = require('../models/Category');
+const Product = require('../src/models/Product');
+const Category = require('../src/models/Category');
 
 const MONGO_URI = process.env.MONGO_URI;
 
